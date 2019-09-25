63e4dac18f2a8a82daa3e983e1a96840947dc8ec
AlfonsoLanderos <alanderos@ucla.edu>
2019-09-24 23:47:51 -0700

    Implement iterate interface

--- src/lsmr.jl
+++ src/lsmr.jl
@@ -1,6 +1,8 @@
+import Base: iterate
+
 export lsmr, lsmr!
 
-using LinearAlgebra
+using LinearAlgebra, Printf
 
 mutable struct LSMRIterable{matT, adjointT, bvecT <: AbstractVector, xvecT <: AbstractVector, vecAdivT <: AbstractVector, numT <: Real, λnumT <: Real}
     A::matT
@@ -55,6 +57,117 @@ mutable struct LSMRIterable{matT, adjointT, bvecT <: AbstractVector, xvecT <: Ab
     atol::numT
     btol::numT
     ctol::numT
+
+    # matrix-vector products
+    mvps::Int
+    mtvps::Int
+end
+
+##
+## Iterators
+##
+
+@inline converged(p::LSMRIterable) = p.istop ∉ (3, 6, 7)
+
+@inline start(p::LSMRIterable) = 0
+
+@inline done(p::LSMRIterable, iteration::Int) = p.istop != 0
+
+#########################
+# Method Implementation #
+#########################
+
+function iterate(p::LSMRIterable, iteration::Int = start(p))
+    if done(p, iteration) return nothing end
+    # update u and v
+    lsmr_update_u_and_v!(p)
+
+    # Construct rotation Qhat_{k,2k+1}.
+    αhat = hypot(p.αbar, p.λ)
+    chat = p.αbar / αhat
+    shat = p.λ / αhat
+
+    # Use a plane rotation (Q_i) to turn B_i to R_i.
+    ρold = p.ρ
+    p.ρ = hypot(αhat, p.β)
+    c = αhat / p.ρ
+    s = p.β / p.ρ
+    θnew = s * p.α
+    p.αbar = c * p.α
+
+    # Use a plane rotation (Qbar_i) to turn R_i^T to R_i^bar.
+    ρbarold = p.ρbar
+    ζold = p.ζ
+    θbar = p.sbar * p.ρ
+    ρtemp = p.cbar * p.ρ
+    p.ρbar = hypot(p.cbar * p.ρ, θnew)
+    p.cbar = p.cbar * p.ρ / p.ρbar
+    p.sbar = θnew / p.ρbar
+    p.ζ = p.cbar * p.ζbar
+    p.ζbar = - p.sbar * p.ζbar
+
+    # Update h, h_hat, x.
+    p.hbar .= p.hbar .* (-θbar * p.ρ / (ρold * ρbarold)) .+ p.h
+    p.x .+= (p.ζ / (p.ρ * p.ρbar)) * p.hbar
+    p.h .= p.h .* (-θnew / p.ρ) .+ p.v
+
+    ##############################################################################
+    ##
+    ## Estimate of ||r||
+    ##
+    ##############################################################################
+
+    # Apply rotation Qhat_{k,2k+1}.
+    βacute = chat * p.βdd
+    βcheck = - shat * p.βdd
+
+    # Apply rotation Q_{k,k+1}.
+    βhat = c * βacute
+    p.βdd = - s * βacute
+
+    # Apply rotation Qtilde_{k-1}.
+    θtildeold = p.θtilde
+    ρtildeold = hypot(p.ρdold, θbar)
+    ctildeold = p.ρdold / ρtildeold
+    stildeold = θbar / ρtildeold
+    p.θtilde = stildeold * p.ρbar
+    p.ρdold = ctildeold * p.ρbar
+    p.βd = - stildeold * p.βd + ctildeold * βhat
+
+    p.τtildeold = (ζold - θtildeold * p.τtildeold) / ρtildeold
+    τd = (p.ζ - p.θtilde * p.τtildeold) / p.ρdold
+    p.d += abs2(βcheck)
+    p.normr = sqrt(p.d + abs2(p.βd - τd) + abs2(p.βdd))
+
+    # Estimate ||A||.
+    p.normA2 += abs2(p.β)
+    p.normA  = sqrt(p.normA2)
+    p.normA2 += abs2(p.α)
+
+    # Estimate cond(A).
+    p.maxrbar = max(p.maxrbar, ρbarold)
+    if iteration > 1
+        p.minrbar = min(p.minrbar, ρbarold)
+    end
+    p.condA = max(p.maxrbar, ρtemp) / min(p.minrbar, ρtemp)
+
+    ##############################################################################
+    ##
+    ## Test for convergence
+    ##
+    ##############################################################################
+
+    # Compute norms for convergence testing.
+    p.normAr  = abs(p.ζbar)
+    p.normx = norm(p.x)
+
+    # Now use these norms to estimate certain other quantities,
+    # some of which will be small near a solution.
+    istop, anorm, cnorm, rnorm = lsmr_convergence(p, iteration)
+
+    p.istop = istop
+
+    return (anorm, cnorm, rnorm), iteration + 1
 end
 
 function lsmr_iterable!(x, A, b;
@@ -119,6 +232,7 @@ function lsmr_iterable!(x, A, b;
     istop = 0
     normr = β
     normAr = α * β
+    # Exit if b = 0 or A'b = 0.
 
     LSMRIterable(
         A, b, x, λ,   # variables in problem statement
@@ -134,7 +248,8 @@ function lsmr_iterable!(x, A, b;
         # variables for stopping rules
         normb, istop, normr, normAr,
         # user settings
-        maxiter, Tr(atol), Tr(btol), ctol
+        maxiter, Tr(atol), Tr(btol), ctol,
+        1, 1 # mvps = 1, mtvps = 1 due to initialization
     )
 end
 
@@ -206,162 +321,56 @@ function lsmr!(x, A, b;
     )
     history = ConvergenceHistory(partial=!log)
     reserve!(history, [:anorm,:rnorm,:cnorm], maxiter)
+    verbose && @printf("=== lsmr ===\n%4s\t%7s\t\t%7s\t\t%7s\n","iter","anorm","cnorm","rnorm")
 
     iterable = lsmr_iterable!(x, A, b; maxiter = maxiter, kwargs...)
+
+    # initialization uses 1 mat-vec and 1 mat^t-vec operation
+    history.mvps  = 1
+    history.mtvps = 1
 
     history[:atol] = iterable.atol
     history[:btol] = iterable.btol
     history[:ctol] = iterable.ctol
 
-    lsmr_method!(history, iterable, verbose)
+    if iterable.normAr != 0
+        for (iteration, norms) = enumerate(iterable)
+            anorm, cnorm, rnorm = norms
 
-    log && shrink!(history)
+            if log
+                nextiter!(history)
+                push!(history, :anorm, anorm)
+                push!(history, :cnorm, cnorm)
+                push!(history, :rnorm, rnorm)
+            end
+
+            verbose && @printf("%3d\t%1.2e\t%1.2e\t%1.2e\n", iteration, anorm, cnorm, rnorm)
+        end
+    end
+
+    setconv(history, converged(iterable))
+
+    if log
+        history.mvps = iterable.mvps
+        history.mtvps = iterable.mtvps
+        shrink!(history)
+    end
+
+    verbose && @printf("\n")
+
+    x = iterable.x
+
     log ? (x, history) : x
 end
 
-#########################
-# Method Implementation #
-#########################
-
-function lsmr_method!(log::ConvergenceHistory, p::LSMRIterable,
-    verbose
-    )
-    verbose && @printf("=== lsmr ===\n%4s\t%7s\t\t%7s\t\t%7s\n","iter","anorm","cnorm","rnorm")
-
-    T = Adivtype(p.A, p.b)
-    Tr = real(T)
-
-    iter = 0
-    # Exit if b = 0 or A'b = 0.
-
-    log.mvps=1
-    log.mtvps=1
-
-    if p.normAr != 0
-        while iter < p.maxiter
-            nextiter!(log, mvps=1)
-            iter += 1
-
-            # update u and v
-            lsmr_update_u_and_v!(log, p)
-
-            # Construct rotation Qhat_{k,2k+1}.
-            αhat = hypot(p.αbar, p.λ)
-            chat = p.αbar / αhat
-            shat = p.λ / αhat
-
-            # Use a plane rotation (Q_i) to turn B_i to R_i.
-            ρold = p.ρ
-            p.ρ = hypot(αhat, p.β)
-            c = αhat / p.ρ
-            s = p.β / p.ρ
-            θnew = s * p.α
-            p.αbar = c * p.α
-
-            # Use a plane rotation (Qbar_i) to turn R_i^T to R_i^bar.
-            ρbarold = p.ρbar
-            ζold = p.ζ
-            θbar = p.sbar * p.ρ
-            ρtemp = p.cbar * p.ρ
-            p.ρbar = hypot(p.cbar * p.ρ, θnew)
-            p.cbar = p.cbar * p.ρ / p.ρbar
-            p.sbar = θnew / p.ρbar
-            p.ζ = p.cbar * p.ζbar
-            p.ζbar = - p.sbar * p.ζbar
-
-            # Update h, h_hat, x.
-            p.hbar .= p.hbar .* (-θbar * p.ρ / (ρold * ρbarold)) .+ p.h
-            p.x .+= (p.ζ / (p.ρ * p.ρbar)) * p.hbar
-            p.h .= p.h .* (-θnew / p.ρ) .+ p.v
-
-            ##############################################################################
-            ##
-            ## Estimate of ||r||
-            ##
-            ##############################################################################
-
-            # Apply rotation Qhat_{k,2k+1}.
-            βacute = chat * p.βdd
-            βcheck = - shat * p.βdd
-
-            # Apply rotation Q_{k,k+1}.
-            βhat = c * βacute
-            p.βdd = - s * βacute
-
-            # Apply rotation Qtilde_{k-1}.
-            θtildeold = p.θtilde
-            ρtildeold = hypot(p.ρdold, θbar)
-            ctildeold = p.ρdold / ρtildeold
-            stildeold = θbar / ρtildeold
-            p.θtilde = stildeold * p.ρbar
-            p.ρdold = ctildeold * p.ρbar
-            p.βd = - stildeold * p.βd + ctildeold * βhat
-
-            p.τtildeold = (ζold - θtildeold * p.τtildeold) / ρtildeold
-            τd = (p.ζ - p.θtilde * p.τtildeold) / p.ρdold
-            p.d += abs2(βcheck)
-            p.normr = sqrt(p.d + abs2(p.βd - τd) + abs2(p.βdd))
-
-            # Estimate ||A||.
-            p.normA2 += abs2(p.β)
-            p.normA  = sqrt(p.normA2)
-            p.normA2 += abs2(p.α)
-
-            # Estimate cond(A).
-            p.maxrbar = max(p.maxrbar, ρbarold)
-            if iter > 1
-                p.minrbar = min(p.minrbar, ρbarold)
-            end
-            p.condA = max(p.maxrbar, ρtemp) / min(p.minrbar, ρtemp)
-
-            ##############################################################################
-            ##
-            ## Test for convergence
-            ##
-            ##############################################################################
-
-            # Compute norms for convergence testing.
-            p.normAr  = abs(p.ζbar)
-            p.normx = norm(p.x)
-
-            # Now use these norms to estimate certain other quantities,
-            # some of which will be small near a solution.
-            test1 = p.normr / p.normb
-            test2 = p.normAr / (p.normA * p.normr)
-            test3 = inv(p.condA)
-            push!(log, :cnorm, test3)
-            push!(log, :anorm, test2)
-            push!(log, :rnorm, test1)
-            verbose && @printf("%3d\t%1.2e\t%1.2e\t%1.2e\n",iter,test2,test3,test1)
-
-            t1 = test1 / (one(Tr) + p.normA * p.normx / p.normb)
-            rtol = p.btol + p.atol * p.normA * p.normx / p.normb
-            # The following tests guard against extremely small values of
-            # atol, btol or ctol.  (The user may have set any or all of
-            # the parameters atol, btol, conlim  to 0.)
-            # The effect is equivalent to the normAl tests using
-            # atol = eps,  btol = eps,  conlim = 1/eps.
-            if iter >= p.maxiter p.istop = 7; break end
-            if 1 + test3 <= 1 p.istop = 6; break end
-            if 1 + test2 <= 1 p.istop = 5; break end
-            if 1 + t1 <= 1 p.istop = 4; break end
-            # Allow for tolerances set by the user.
-            if test3 <= p.ctol p.istop = 3; break end
-            if test2 <= p.atol p.istop = 2; break end
-            if test1 <= rtol  p.istop = 1; break end
-        end
-    end
-    verbose && @printf("\n")
-    setconv(log, p.istop ∉ (3, 6, 7))
-    p.x
-end
-
 function lsmr_initialize_u_and_v!(u, tmp_u, v, tmp_v, A, adjointA, x)
+    # perform 1 matrix-vector product to update u
     mul!(tmp_u, A, x)
     u .-= tmp_u
     β = norm(u)
     u .*= inv(β)
 
+    # perform 1 adjoint-vector product to update v
     mul!(v, adjointA, u)
     α = norm(v)
     v .*= inv(α)
@@ -369,13 +378,16 @@ function lsmr_initialize_u_and_v!(u, tmp_u, v, tmp_v, A, adjointA, x)
     return u, tmp_u, v, tmp_v, α, β
 end
 
-function lsmr_update_u_and_v!(log, p::LSMRIterable)
+function lsmr_update_u_and_v!(p::LSMRIterable)
+    # perform 1 matrix-vector product to update u
+    p.mvps += 1
     mul!(p.tmp_u, p.A, p.v)
     p.u .= p.tmp_u .+ p.u .* -(p.α)
     p.β = norm(p.u)
 
     if p.β > 0
-        log.mtvps += 1
+        # perform 1 adjoint-vector product to update v
+        p.mtvps += 1
         p.u .*= inv(p.β)
         mul!(p.tmp_v, p.adjointA, p.u)
         p.v .= p.tmp_v .+ p.v .* -(p.β)
@@ -384,4 +396,35 @@ function lsmr_update_u_and_v!(log, p::LSMRIterable)
     end
 
     return nothing
+end
+
+function lsmr_convergence(p::LSMRIterable, iteration::Int=0)
+    Tr = real(eltype(p.b))
+
+    test1 = p.normr / p.normb # rnorm
+    test2 = p.normAr / (p.normA * p.normr) # anorm
+    test3 = inv(p.condA) # cnorm
+
+    t1 = test1 / (one(Tr) + p.normA * p.normx / p.normb)
+    rtol = p.btol + p.atol * p.normA * p.normx / p.normb
+
+    # The following tests guard against extremely small values of
+    # atol, btol or ctol.  (The user may have set any or all of
+    # the parameters atol, btol, conlim  to 0.)
+    # The effect is equivalent to the normAl tests using
+    # atol = eps,  btol = eps,  conlim = 1/eps.
+
+    istop = 0
+
+    if iteration >= p.maxiter istop = 7 end
+    if 1 + test3 <= 1 istop = 6 end
+    if 1 + test2 <= 1 istop = 5 end
+    if 1 + t1 <= 1 istop = 4 end
+
+    # Allow for tolerances set by the user.
+    if test3 <= p.ctol istop = 3 end
+    if test2 <= p.atol istop = 2 end
+    if test1 <= rtol istop = 1 end
+
+    return istop, test2, test3, test1
 end
